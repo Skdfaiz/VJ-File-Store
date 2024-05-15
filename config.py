aeb650ab822b1e532c92651225482f8b13d810fb
Skdfaiz <faizalamskd92@gmail.com>
2024-05-16 00:23:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -50,8 +50,8 @@ BOT_USERNAME = environ.get("BOT_USERNAME", "Battlethroughtheheavens_bot") # your
 PICS = (environ.get('PICS', 'https://graph.org/file/82ef767ffebe3a948e476.jpg https://graph.org/file/82ef767ffebe3a948e476.jpg')).split() # Bot Start Picture
 
 # Auto Delete Information
-AUTO_DELETE = int(environ.get("AUTO_DELETE", "")) # Time in Minutes
-AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "")) # Time in Seconds
+AUTO_DELETE = int(environ.get("AUTO_DELETE", "999999")) # Time in Minutes
+AUTO_DELETE_TIME = int(environ.get("AUTO_DELETE_TIME", "999999")) # Time in Seconds
 
 # Channel Information
 LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002089820006"))
